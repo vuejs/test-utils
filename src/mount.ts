3b5c0dd5d5b50363a17a9cac60819947036d23d7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-03-20 17:30:14 +1000

    fix: add comments

--- src/mount.ts
+++ src/mount.ts
@@ -1,7 +1,7 @@
-import { h, createApp, VNode, defineComponent, getCurrentInstance } from 'vue'
+import { h, createApp, VNode, defineComponent } from 'vue'
 
 import { VueWrapper, createWrapper } from './vue-wrapper'
-import { createEmitMixin } from './emitMixin';
+import { createEmitMixin } from './emitMixin'
 
 type Slot = VNode | string
 
@@ -18,41 +18,49 @@ interface MountingOptions<Props> {
 
 export function mount<P>(
   component: any,
-  // component: new () => ComponentPublicInstance<P>,
   options?: MountingOptions<P>
 ): VueWrapper {
 
+  // Reset the document.body
   document.getElementsByTagName('html')[0].innerHTML = '';
   const el = document.createElement('div')
   el.id = 'app'
   document.body.appendChild(el)
 
+  // handle any slots passed via mounting options
   const slots = options?.slots &&
     Object.entries(options.slots).reduce<Record<string, () => VNode | string>>((acc, [name, fn]) => {
     acc[name] = () => fn
     return acc
   }, {})
 
+  // create the wrapper component
   const Parent = (props?: P) => defineComponent({
     render() {
       return h(component, props, slots)
     }
   })
 
+  // create the vm
   const vm = createApp(Parent(options && options.props))
 
-  const { emitMixin, events } = createEmitMixin()
-  if (options.plugins) {
+  // use and plugins from mounting options
+  if (options?.plugins) {
     for (const use of options.plugins)
     vm.use(use)
   }
 
-  if (options.mixins) {
+  // use any mixins from mounting options
+  if (options?.mixins) {
     for (const mixin of options.mixins)
     vm.mixin(mixin)
   }
 
+  // add tracking for emitted events
+  const { emitMixin, events } = createEmitMixin()
   vm.mixin(emitMixin)
+
+  // mount the app!
   const app = vm.mount('#app')
 
   return createWrapper(app, events)
