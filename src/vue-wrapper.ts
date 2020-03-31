ecf03262bc8ba7260a0a93b98fc27ed1ceafaaeb
Dobromir Hristov <dobromir92@gmail.com>
2020-03-31 16:36:01 +0300

    Update src/vue-wrapper.ts

--- src/vue-wrapper.ts
+++ src/vue-wrapper.ts
@@ -28,7 +28,7 @@ export class VueWrapper implements WrapperAPI {
   }
 
   get element() {
-    // get the parent element of the current component
+    // if the component has multiple root elements, we use the parent's element 
     return this.hasMultipleRoots ? this.parentElement : this.componentVM.$el
   }
 
