5b42f424281687f36f366e980882e5c4e4de60d8
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-04-05 13:05:29 +1000

    feat: support SFC as slot mounting option

---
+++ tests/components/WithProps.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>
+    {{ msg }}
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Hello',
+
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
