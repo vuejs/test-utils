26e6a1e342d66686983e163e13e3363e7e993fe7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-04-10 12:32:03 +1000

    tests: add tests for setProps and watcher

--- tests/setProps.spec.ts
+++ tests/setProps.spec.ts
@@ -1,12 +1,11 @@
 import { mount } from '../src'
 
 describe('setProps', () => {
-  it('updates a prop', async () => {
+  it('updates a primitive prop', async () => {
     const Foo = {
       props: ['foo'],
       template: '<div>{{ foo }}</div>'
     }
-
     const { wrapper, setProps } = mount(Foo, {
       props: {
         foo: 'foo'
@@ -16,5 +15,63 @@ describe('setProps', () => {
 
     await setProps({ foo: 'qux' })
     expect(wrapper.html()).toContain('qux')
+  })
+
+  it('updates a function prop', async () => {
+    const Foo = {
+      props: ['obj'],
+      template: `
+        <div>
+          <div v-if="obj.foo()">foo</div>
+        </div>
+      `
+    }
+    const { wrapper, setProps } = mount(Foo, {
+      props: {
+        obj: {
+          foo: () => true
+        }
+      }
+    })
+    expect(wrapper.html()).toContain('foo')
+
+    await setProps({ obj: { foo: () => false } })
+    expect(wrapper.html()).not.toContain('foo')
+  })
+
+  it('sets component props, and updates DOM when props were not initially passed', async () => {
+    const Foo = {
+      props: ['foo'],
+      template: `<div>{{ foo }}</div>`
+    }
+    const { wrapper, setProps } = mount(Foo)
+    expect(wrapper.html()).not.toContain('foo')
+
+    await setProps({ foo: 'foo' })
+
+    expect(wrapper.html()).toContain('foo')
+  })
+
+  it('triggers a watcher', async () => {
+    const Foo = {
+      props: ['foo'],
+      data() {
+        return {
+          bar: 'original-bar'
+        }
+      },
+      watch: {
+        foo(val: string) {
+          this.bar = val
+        }
+      },
+      template: `<div>{{ bar }}</div>`
+    }
+    const { wrapper, setProps } = mount(Foo)
+    expect(wrapper.html()).toContain('original-bar')
+
+    await setProps({ foo: 'updated-bar' })
+
+    expect(wrapper.html()).toContain('updated-bar')
   })
 })
