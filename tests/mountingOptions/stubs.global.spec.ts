361019d28a9cb29814ed357aece64d43bbb800da
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-04-13 13:37:20 +1000

    wip: tests for stubs

--- tests/mountingOptions/stubs.global.spec.ts
+++ tests/mountingOptions/stubs.global.spec.ts
@@ -1,9 +1,9 @@
-import { h, ComponentOptions } from 'vue'
+import { h, ComponentOptions, onMounted, onBeforeMount } from 'vue'
 
 import { mount } from '../../src'
 
 describe('mounting options: stubs', () => {
-  it('stubs a component globally via `stubs`', () => {
+  it('stubs when a root node is present', () => {
     const Foo = {
       name: 'Foo',
       render() {
@@ -12,7 +12,7 @@ describe('mounting options: stubs', () => {
     }
     const Component: ComponentOptions = {
       render() {
-        return h('div', [h('span'), h(Foo)])
+        return h('div', h(Foo))
       }
     }
 
@@ -24,6 +24,59 @@ describe('mounting options: stubs', () => {
       }
     })
 
-    console.log(wrapper.html())
+    expect(wrapper.html()).toBe('<div><foo-stub></foo-stub></div>')
+  })
+
+  it('stubs in a fragment', () => {
+    const Foo = {
+      name: 'Foo',
+      render() {
+        return h('p')
+      }
+    }
+    const Component: ComponentOptions = {
+      render() {
+        return h(() => [h('div'), h(Foo)])
+      }
+    }
+
+    const wrapper = mount(Component, {
+      global: {
+        stubs: {
+          Foo: true
+        }
+      }
+    })
+
+    expect(wrapper.html()).toBe('<div></div><foo-stub></foo-stub>')
+  })
+
+  it('prevents lifecycle hooks triggering in a stub', () => {
+    const onBeforeMount = jest.fn()
+    const beforeCreate = jest.fn()
+    const Foo = {
+      name: 'Foo',
+      setup() {
+        onBeforeMount(onBeforeMount)
+        return () => h('div')
+      },
+      beforeCreate
+    }
+    const Comp = {
+      render() {
+        return h(Foo)
+      }
+    }
+
+    mount(Comp, {
+      global: {
+        stubs: {
+          Foo: true
+        }
+      }
+    })
+
+    expect(onBeforeMount).not.toHaveBeenCalled()
+    expect(beforeCreate).not.toHaveBeenCalled()
   })
 })
