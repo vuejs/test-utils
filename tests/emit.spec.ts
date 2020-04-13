ed0158d69517f07acbe6ca76317e6e6e7ca76e7d
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-04-13 21:12:32 +1000

    refactor: simplify emit mixin

--- tests/emit.spec.ts
+++ tests/emit.spec.ts
@@ -44,7 +44,7 @@ describe('emitted', () => {
     expect(wrapper.emitted().hello[1]).toEqual(['foo', 'bar'])
   })
 
-  it.only('captures events emitted via destructured emit', () => {
+  it('captures events emitted via destructured emit', () => {
     const Component = defineComponent({
       name: 'ContextEmit',
 
