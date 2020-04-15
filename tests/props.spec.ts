67dc27e231ba4ba352c40ea85f0c846ce99bf965
dobromir-hristov <dobromir92@gmail.com>
2020-04-16 00:09:22 +0300

    tests: improve tests

--- tests/props.spec.ts
+++ tests/props.spec.ts
@@ -1,5 +1,6 @@
 import { mount } from '../src'
 import WithProps from './components/WithProps.vue'
+import Hello from './components/Hello.vue'
 
 describe('props', () => {
   it('returns a single prop applied to a component', () => {
@@ -10,5 +11,15 @@ describe('props', () => {
   it('returns all props applied to a component', () => {
     const wrapper = mount(WithProps, { props: { msg: 'ABC' } })
     expect(wrapper.props()).toEqual({ msg: 'ABC' })
+  })
+
+  it('returns undefined if props does not exist', () => {
+    const wrapper = mount(WithProps, { props: { msg: 'ABC' } })
+    expect(wrapper.props('foo')).toEqual(undefined)
+  })
+
+  it('returns empty object for components without props', () => {
+    const wrapper = mount(Hello)
+    expect(wrapper.props()).toEqual({})
   })
 })
