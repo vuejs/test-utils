407a7570396646b518476cda19a10d04016b56f9
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-04-24 21:25:55 +1000

    fix: types

--- src/types.ts
+++ src/types.ts
@@ -27,19 +27,6 @@ interface NameSelector {
   name: string
 }
 
-export type FindComponentSelector = RefSelector | NameSelector | string
-export type FindAllComponentsSelector = NameSelector | string
-
-export type GlobalMountOptions = {
-  plugins?: Plugin[]
-  mixins?: ComponentOptions[]
-  mocks?: Record<string, any>
-  provide?: Record<any, any>
-  components?: Record<string, Component | object>
-  directives?: Record<string, Directive>
-  stubs?: Record<any, any>
-}
-
 interface RefSelector {
   ref: string
 }
