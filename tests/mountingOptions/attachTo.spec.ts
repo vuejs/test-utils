263b8acf3e9332ba22836289c786d6335f643fc6
pikax <carlos@hypermob.co.uk>
2020-04-28 12:23:34 +0100

    clean commented code

--- tests/mountingOptions/attachTo.spec.ts
+++ tests/mountingOptions/attachTo.spec.ts
@@ -2,7 +2,6 @@ import { mount } from '../../src'
 
 const innerHTML = '<input><span>Hello world</span>'
 const outerHTML = `<div id="attach-to">${innerHTML}</div>`
-// const ssrHTML = `<div id="attach-to" data-server-rendered="true">${innerHTML}</div>`
 const template = '<div id="attach-to"><input /><span>Hello world</span></div>'
 const TestComponent = { template }
 
@@ -47,25 +46,4 @@ describe('options.attachTo', () => {
   })
 
   it.todo('correctly hydrates markup')
-
-  // it('correctly hydrates markup', () => {
-  //   expect(document.getElementById('attach-to')).toBeNull()
-
-  //   const div = document.createElement('div')
-  //   div.id = 'attach-to'
-  //   div.setAttribute('data-server-rendered', 'true')
-  //   div.innerHTML = innerHTML
-  //   document.body.appendChild(div)
-  //   expect(div.outerHTML).toBe(ssrHTML)
-  //   const wrapper = mount(TestComponent, {
-  //     attachTo: '#attach-to'
-  //   })
-
-  //   const rendered = document.getElementById('attach-to')
-  //   expect(wrapper.vm.$el.parentNode).not.toBeNull()
-  //   expect(rendered).not.toBeNull()
-  //   expect(rendered.outerHTML).toBe(outerHTML)
-  //   wrapper.unmount()
-  //   expect(document.getElementById('attach-to')).toBeNull()
-  // })
 })
