fc216ba458809ed879065952a53ed96e664d80cf
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-04-28 21:23:46 +1000

    feat: export DOMWrapper

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { mount } from './mount'
 import { RouterLinkStub } from './components/RouterLinkStub'
 import { VueWrapper } from './vue-wrapper'
+import { DOMWrapper } from './dom-wrapper'
 import { config } from './config'
 
-export { mount, RouterLinkStub, VueWrapper, config }
+export { mount, RouterLinkStub, VueWrapper, config, DOMWrapper }
