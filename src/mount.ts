ff8170c047a710e5f257cdd94f7ca8c7e0fd6bc8
Chris Vincent <chris@cvbytes.com>
2020-05-11 03:43:29 -0600

    fix: add slot dictionary type as typescript string index assignment workaround

--- src/mount.ts
+++ src/mount.ts
@@ -29,13 +29,16 @@ import { stubComponents } from './stubs'
 
 type Slot = VNode | string | { render: Function }
 
+type SlotDictionary = {
+  [key: string]: Slot
+}
+
 interface MountingOptions<Props> {
   data?: () => Record<string, unknown>
   props?: Props
   attrs?: Record<string, unknown>
-  slots?: {
+  slots?: SlotDictionary & {
     default?: Slot
-    [key: string]: Slot
   }
   global?: GlobalMountOptions
   attachTo?: HTMLElement | string
