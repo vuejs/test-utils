3a204be8ddf38485fd7af56e522e246ec4123f03
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-05-12 23:37:00 +1000

    fix: support global stubs

--- src/utils.ts
+++ src/utils.ts
@@ -19,15 +19,38 @@ function mergeGlobalProperties(
   const mixins = [...configMixins, ...mountMixins]
   const plugins = [...configPlugins, ...mountPlugins]
 
+  const stubs: Record<string, any> = {}
+
+  if (configRest.stubs) {
+    if (Array.isArray(configRest.stubs)) {
+      configRest.stubs.forEach((x) => (stubs[x] = true))
+    } else {
+      for (const [k, v] of Object.entries(configRest.stubs)) {
+        stubs[k] = v
+      }
+    }
+  }
+
+  if (mountRest.stubs) {
+    if (mountRest.stubs && Array.isArray(mountRest.stubs)) {
+      mountRest.stubs.forEach((x) => (stubs[x] = true))
+    } else {
+      for (const [k, v] of Object.entries(mountRest.stubs)) {
+        stubs[k] = v
+      }
+    }
+  }
+
   return {
     mixins,
     plugins,
+    stubs,
     components: { ...configRest.components, ...mountRest.components },
     provide: { ...configRest.provide, ...mountRest.provide },
     mocks: { ...configRest.mocks, ...mountRest.mocks },
     config: { ...configRest.config, ...mountRest.config },
-    directives: { ...configRest.directives, ...mountRest.directives },
-    stubs: { ...configRest.stubs, ...mountRest.stubs }
+    directives: { ...configRest.directives, ...mountRest.directives }
+    // stubs: { configRest.stubs, ...mountRest.stubs }
   }
 }
 
