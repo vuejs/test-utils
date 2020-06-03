4c6739bd4d485ff5423f8afe2e8a28caaa57dcc1
cexbrayat <cedric@ninja-squad.com>
2020-06-03 18:05:00 +0200

    chore(stubs): proper typings

--- src/dataMixin.ts
+++ src/dataMixin.ts
@@ -4,7 +4,7 @@ export const createDataMixin = (data: Record<string, unknown>) => {
   return {
     created() {
       for (const [k, v] of Object.entries(data)) {
-        getCurrentInstance().data = { ...getCurrentInstance().data, [k]: v }
+        getCurrentInstance()!.data = { ...getCurrentInstance()!.data, [k]: v }
       }
     }
   }
