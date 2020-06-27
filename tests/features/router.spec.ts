5535316281a19b352bd39e6d7e253a26749eabcd
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-06-27 23:00:22 +1000

    tests: add more failing cases

--- tests/features/router.spec.ts
+++ tests/features/router.spec.ts
@@ -1,34 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { h } from 'vue'
+import { h, createApp } from 'vue'
 import WithRouter from '../components/WithRouter.vue'
 import { mount } from '../../src'
 
-describe('vue-router', () => {
-  it('works without problems', async () => {
-    const router = () => {
-      return createRouter({
-        history: createWebHistory(),
-        routes: [
-          {
-            path: '/posts',
-            component: {
-              render() {
-                return h('div', 'This is the posts route')
-              }
-            }
-          },
-          {
-            path: '/',
-            component: {
-              render() {
-                return h('div', 'This is this home route')
-              }
-            }
+const router = () => {
+  return createRouter({
+    history: createWebHistory(),
+    routes: [
+      {
+        path: '/posts',
+        component: {
+          render() {
+            return h('div', 'This is the posts route')
           }
-        ]
-      })
-    }
+        }
+      },
+      {
+        path: '/',
+        component: {
+          render() {
+            return h('div', 'This is this home route')
+          }
+        }
+      }
+    ]
+  })
+}
 
+describe('vue-router', () => {
+  it('may not be a VTU issue?', async () => {
+    const el = document.createElement('div')
+    el.id = 'app'
+    document.body.appendChild(el)
+    const app = createApp(WithRouter)
+    app.use(router())
+    app.mount('#app')
+    console.log(document.body.innerHTML)
+  })
+
+  it('router-view is not rendered', async () => {
     window.location.href = '/'
     const wrapper = mount(WithRouter, {
       global: {
