db5f580099bacf48be2f7ebf453b57e6b1e30644
cuixiaorui <cui_xiaorui@126.com>
2020-07-13 19:17:44 +0800

    chore: remove ShapeFlags

--- src/utils/vueShared.ts
+++ src/utils/vueShared.ts
@@ -19,17 +19,3 @@ const hyphenateRE = /\B([A-Z])/g
 export const hyphenate = cacheStringFunction((str: string): string => {
   return str.replace(hyphenateRE, '-$1').toLowerCase()
 })
-
-export const enum ShapeFlags {
-  ELEMENT = 1,
-  FUNCTIONAL_COMPONENT = 1 << 1,
-  STATEFUL_COMPONENT = 1 << 2,
-  TEXT_CHILDREN = 1 << 3,
-  ARRAY_CHILDREN = 1 << 4,
-  SLOTS_CHILDREN = 1 << 5,
-  TELEPORT = 1 << 6,
-  SUSPENSE = 1 << 7,
-  COMPONENT_SHOULD_KEEP_ALIVE = 1 << 8,
-  COMPONENT_KEPT_ALIVE = 1 << 9,
-  COMPONENT = ShapeFlags.STATEFUL_COMPONENT | ShapeFlags.FUNCTIONAL_COMPONENT
-}
