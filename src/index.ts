fd3d26cb842ee48f639e9091def719822599d8b2
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-07-15 20:37:41 +1000

    feat: add flush promises

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,14 @@ import { RouterLinkStub } from './components/RouterLinkStub'
 import { VueWrapper } from './vue-wrapper'
 import { DOMWrapper } from './dom-wrapper'
 import { config } from './config'
+import { flushPromises } from './utils/flushPromises'
 
-export { mount, shallowMount, RouterLinkStub, VueWrapper, DOMWrapper, config }
+export {
+  mount,
+  shallowMount,
+  RouterLinkStub,
+  VueWrapper,
+  DOMWrapper,
+  config,
+  flushPromises
+}
