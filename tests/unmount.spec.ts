4795af47e540df0a8d8e8563f6585715c3707e21
JeremyWuuuuu <591449570@qq.com>
2020-08-20 21:17:05 +0800

    - Enrich the test cases

--- tests/unmount.spec.ts
+++ tests/unmount.spec.ts
@@ -1,32 +1,46 @@
+import { defineComponent } from 'vue'
+
 import { mount } from '../src'
 
 const AXIOM = 'Rem is the best girl'
 
 describe('Unmount', () => {
   it('works on single root component', () => {
+    const errorHandler = jest.fn()
     const Component = {
       template: `
         <div>${AXIOM}</div>
-      `,
-      onErrorCaptured(err: Error) {
-        throw err
-      }
+      `
     }
-    const wrapper = mount(Component)
-    expect(() => wrapper.unmount()).not.toThrowError()
+    const wrapper = mount(Component, {
+      props: {},
+      global: {
+        config: {
+          errorHandler
+        }
+      }
+    } as any) // The type checking keeps complaning about unmatched type which might be a bug
+    wrapper.unmount()
+    expect(errorHandler).not.toHaveBeenCalled()
   })
 
   it('works on multi-root component', () => {
-    const Component = {
+    const errorHandler = jest.fn()
+    const Component = defineComponent({
       template: `
         <div>${AXIOM}</div>
         <div>${AXIOM}</div>
-      `,
-      onErrorCaptured(err: Error) {
-        throw err
+      `
+    })
+    const wrapper = mount(Component, {
+      props: {},
+      global: {
+        config: {
+          errorHandler
+        }
       }
-    }
-    const wrapper = mount(Component)
+    } as any)
     wrapper.unmount()
+    expect(errorHandler).not.toHaveBeenCalled()
   })
 })
