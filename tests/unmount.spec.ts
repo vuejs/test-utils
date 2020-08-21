15fdd8730e736e1d14b4a36981669760ed9c9f6c
JeremyWuuuuu <591449570@qq.com>
2020-08-21 20:19:05 +0800

    Remove mounting text

--- tests/unmount.spec.ts
+++ tests/unmount.spec.ts
@@ -2,14 +2,12 @@ import { defineComponent } from 'vue'
 
 import { mount } from '../src'
 
-const AXIOM = 'Rem is the best girl'
-
 describe('Unmount', () => {
   it('works on single root component', () => {
     const errorHandler = jest.fn()
     const Component = {
       template: `
-        <div>${AXIOM}</div>
+        <div></div>
       `
     }
     const wrapper = mount(Component, {
@@ -28,8 +26,8 @@ describe('Unmount', () => {
     const errorHandler = jest.fn()
     const Component = defineComponent({
       template: `
-        <div>${AXIOM}</div>
-        <div>${AXIOM}</div>
+        <div></div>
+        <div></div>
       `
     })
     const wrapper = mount(Component, {
