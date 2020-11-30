9962e5bbbfe78c2d33b55f23f10affc798daa4e3
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-11-30 16:24:45 +1000

    tests: add test for latest <script setup>

--- tests/features/sfc.spec.ts
+++ tests/features/sfc.spec.ts
@@ -1,9 +1,17 @@
 import { mount } from '../../src'
 import Hello from '../components/Hello.vue'
+import ScriptSetup from '../components/ScriptSetup.vue'
 
 describe('sfc', () => {
   it('mounts an sfc via vue-test-transformer', () => {
     const wrapper = mount(Hello)
     expect(wrapper.find('#msg').text()).toBe('Hello world')
+  })
+
+  // rfc: https://github.com/vuejs/rfcs/pull/228
+  it('works with <script setup> (as of Vue 3.0.3)', async () => {
+    const wrapper = mount(ScriptSetup)
+    await wrapper.find('button').trigger('click')
+    expect(wrapper.html()).toContain('1')
   })
 })
