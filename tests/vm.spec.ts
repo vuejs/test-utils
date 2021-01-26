3cd66869a7d55ac2b59c285971bc0a3a9d2fecdb
cexbrayat <cedric@ninja-squad.com>
2021-01-26 16:29:55 +0100

    repro: spy on vm

--- tests/vm.spec.ts
+++ tests/vm.spec.ts
@@ -24,4 +24,25 @@ describe('vm', () => {
 
     expect(wrapper.vm.isEnabled).toBe(false)
   })
+
+  it('allows spying on vm', async () => {
+    const Component = defineComponent({
+      name: 'VTUComponent',
+      template: '<div @click="toggle()">{{ msg }}</div>',
+      setup() {
+        const msg = 'hello'
+        const isEnabled = ref(true)
+        const toggle = () => (isEnabled.value = !isEnabled.value)
+        return { msg, isEnabled, toggle }
+      }
+    })
+
+    const wrapper = mount(Component)
+
+    jest.spyOn(wrapper.vm, 'toggle')
+
+    await wrapper.get('div').trigger('click')
+
+    expect(wrapper.vm.toggle).toHaveBeenCalled()
+  })
 })
