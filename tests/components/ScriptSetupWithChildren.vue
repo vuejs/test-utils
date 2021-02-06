d8251e79e48bfc441769e65f61d968f518ddb1df
rori <mail@rori.info>
2021-02-06 16:20:46 +0100

    fix: shallowMount failed for script setup components

---
+++ tests/components/ScriptSetupWithChildren.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Hello from './Hello'
+import ComponentWithInput from './ComponentWithInput'
+import ComponentWithoutName from './ComponentWithoutName'
+import ScriptSetup from './ScriptSetup'
+import WithProps from './WithProps'
+</script>
+
+<template>
+  <hello />
+  <component-with-input />
+  <component-without-name />
+  <script-setup />
+  <with-props />
+</template>
