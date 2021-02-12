8889338b1565e28285d3125d2d0e1f1910fa19d0
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2021-02-12 23:30:47 +0000

    fix: linting issues

--- src/utils.ts
+++ src/utils.ts
@@ -31,9 +31,10 @@ export function mergeGlobalProperties(
     mocks: { ...configGlobal.mocks, ...mountGlobal.mocks },
     config: { ...configGlobal.config, ...mountGlobal.config },
     directives: { ...configGlobal.directives, ...mountGlobal.directives },
-    renderStubDefaultSlot: mountGlobal.renderStubDefaultSlot !== undefined
-      ? mountGlobal.renderStubDefaultSlot
-      : configGlobal.renderStubDefaultSlot
+    renderStubDefaultSlot:
+      mountGlobal.renderStubDefaultSlot !== undefined
+        ? mountGlobal.renderStubDefaultSlot
+        : configGlobal.renderStubDefaultSlot
   }
 }
 
