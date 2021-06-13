ed4affd002c287fca6bb179d2b115aed9df5410b
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-13 21:20:20 +1000

    fix: capture emitted events from script setup components

---
+++ tests/components/EmitsEventScriptSetup.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="click" />
+</template>
+
+<script setup lang="ts">
+import { onMounted, defineEmit } from 'vue';
+
+const emit = defineEmit(['bar']);
+
+const click = () => emit('bar', 'click')
+
+onMounted(() => {
+  emit('bar', 'mounted')
+})
+</script>
