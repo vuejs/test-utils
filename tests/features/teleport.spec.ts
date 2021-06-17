7cf22de2d83125739b348f5df3c829a8f1677188
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-06-17 14:39:05 +1000

    add example with getComponent

--- tests/features/teleport.spec.ts
+++ tests/features/teleport.spec.ts
@@ -1,4 +1,4 @@
-import { defineComponent, FunctionalComponent, h, Teleport } from 'vue'
+import { defineComponent, h, Teleport } from 'vue'
 import { mount } from '../../src'
 
 describe('teleport', () => {
@@ -25,21 +25,32 @@ describe('teleport', () => {
     )
   })
 
+  const Greeter = defineComponent({
+    emits: {
+      greet: (msg: string) => {
+        return true
+      }
+    },
+    props: {
+      msg: {
+        type: String,
+        required: true
+      }
+    },
+    setup({ msg }, { emit }) {
+      return () =>
+        h(
+          'button',
+          { onClick: () => emit('greet', `${msg.toUpperCase()}!!!`) },
+          `${msg}!!!`
+        )
+    }
+  })
+
   it('teleports a component with props', async () => {
     const destination = document.createElement('div')
     destination.id = 'far-away'
     document.body.appendChild(destination)
-
-    const Greeter: FunctionalComponent<
-      { msg: string },
-      { greet: (msg: string) => void }
-    > = ({ msg }, { emit }) => {
-      return h(
-        'button',
-        { onClick: () => emit('greet', `${msg.toUpperCase()}!!!`) },
-        `${msg}!!!`
-      )
-    }
 
     const onGreet = jest.fn()
 
@@ -60,6 +71,42 @@ describe('teleport', () => {
     document.querySelector<HTMLButtonElement>('button')!.click()
 
     expect(onGreet).toHaveBeenCalledWith('HELLO!!!')
+    expect(document.body.outerHTML).toBe(
+      `<body><div id="far-away"><button>Hello!!!</button></div></body>`
+    )
+  })
+
+  it('teleports a component with props', async () => {
+    const destination = document.createElement('div')
+    destination.id = 'far-away'
+    document.body.appendChild(destination)
+
+    const onGreet = jest.fn()
+
+    const Comp = defineComponent({
+      setup() {
+        return () =>
+          h(() =>
+            h(
+              Teleport,
+              { to: '#far-away' },
+              h(Greeter, { onGreet, msg: 'Hello' })
+            )
+          )
+      }
+    })
+
+    const wrapper = mount(Comp)
+
+    // although <Greeter /> is teleported outside the component,
+    // it's still in the vdom of the wrapper,
+    // so you can just query it like you normally would - pretty neat.
+    await wrapper.getComponent(Greeter).trigger('click')
+
+    expect(onGreet).toHaveBeenCalledWith('HELLO!!!')
+    expect(wrapper.getComponent(Greeter).props()).toEqual({
+      msg: 'Hello'
+    })
 
     expect(document.body.outerHTML).toBe(
       `<body><div id="far-away"><button>Hello!!!</button></div></body>`
