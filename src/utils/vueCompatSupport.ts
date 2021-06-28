8e9218b9546a1e0dc287baffd8fd15597d6dd95a
Illya Klymov <xanf@xanf.me>
2021-06-28 19:53:56 +0300

    feat(compat): correctly stub components, wrapped in Vue.extend

--- src/utils/vueCompatSupport.ts
+++ src/utils/vueCompatSupport.ts
@@ -1,27 +1,34 @@
 import * as Vue from 'vue'
 import type { ComponentOptions } from 'vue'
-import { FindAllComponentsSelector } from '../types'
 import { hasOwnProperty } from '../utils'
 
 function isCompatEnabled(key: string): boolean {
   return (Vue as any).compatUtils?.isCompatEnabled(key) ?? false
 }
 
-export function convertLegacyVueExtendSelector(
-  selector: FindAllComponentsSelector
-): FindAllComponentsSelector {
-  if (!isCompatEnabled('GLOBAL_EXTEND') || typeof selector !== 'function') {
-    return selector
+export function isLegacyExtendedComponent(component: unknown): component is {
+  (): Function
+  super: Function
+  options: ComponentOptions
+} {
+  if (!isCompatEnabled('GLOBAL_EXTEND') || typeof component !== 'function') {
+    return false
   }
 
   // @ts-ignore Vue.extend is part of Vue2 compat API, types are missing
   const fakeCmp = Vue.extend({})
 
-  return hasOwnProperty(selector, 'super') &&
-    hasOwnProperty(selector, 'options') &&
-    fakeCmp.super === selector.super
-    ? (selector.options as FindAllComponentsSelector)
-    : selector
+  return (
+    hasOwnProperty(component, 'super') &&
+    hasOwnProperty(component, 'options') &&
+    fakeCmp.super === component.super
+  )
+}
+
+export function unwrapLegacyVueExtendComponent<T>(
+  selector: T
+): T | ComponentOptions {
+  return isLegacyExtendedComponent(selector) ? selector.options : selector
 }
 
 export function isLegacyFunctionalComponent(component: unknown) {
