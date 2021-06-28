265216512cfbf2111a2f782fd004dc7ed497ae60
Illya Klymov <xanf@xanf.me>
2021-06-28 11:15:43 +0300

    chore: better way to detect legacy components

--- src/utils/vueCompatSupport.ts
+++ src/utils/vueCompatSupport.ts
@@ -1,15 +1,21 @@
+import * as Vue from 'vue'
+import type { ComponentOptions } from 'vue'
 import { FindAllComponentsSelector } from '../types'
 
+function isCompatEnabled(key: string): boolean {
+  return (Vue as any).compatUtils?.isCompatEnabled(key) ?? false
+}
+
 export function convertLegacyVueExtendSelector(
-  selector: any
+  selector: FindAllComponentsSelector
 ): FindAllComponentsSelector {
-  if (
-    typeof selector === 'function' &&
-    selector.name === 'SubVue' &&
-    selector.options
-  ) {
-    return selector.options
+  if (!isCompatEnabled('GLOBAL_EXTEND') || typeof selector !== 'function') {
+    return selector
   }
 
-  return selector
+  // @ts-ignore Vue.extend is part of Vue2 compat API, types are missing
+  const fakeCmp = Vue.extend({})
+
+  // @ts-ignore TypeScript does not allow access of properties on functions
+  return fakeCmp.super === selector.super ? selector.options : selector
 }
