f923e9595b625904cd1afbf8dbe5575b0821e3f4
cexbrayat <cedric@ninja-squad.com>
2021-07-30 16:49:08 +0200

    chore: update to jest 27
    Updates jest, ts-jest and babel-jest to v27.
    vue-jest v5 does not support jest v27,
    so a new vue3-jest package has been introduced (see https://github.com/vuejs/vue-jest/pull/343),
    with a version number aligned on the jest version (as ts-jest and babel-jest do).
    
    This commits replaces vue-jest by vue3-jest, and updates the jest config to use this new trasnformer.
    This will be the default Vue CLI setup in the future.

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
     '^.+\\js$': 'babel-jest'
   },
   moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
