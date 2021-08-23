cd223f9ca4f737e9602fb3d6317da81480400957
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-08-24 09:57:57 +1000

    chore: add example for script setpup

---
+++ tests/features/ScriptSetup_ToRefsInject.spec.ts
@@ -0,0 +1,21 @@
+import { mount } from '../../src'
+import ScriptSetup_ToRefsInject from '../components/ScriptSetup_ToRefsInject.vue'
+
+it('', async () => {
+  const wrapper = mount(ScriptSetup_ToRefsInject, {
+    props: {
+      title: 'Some title'
+    },
+    global: {
+      provide: {
+        parentType: 'Parent Type'
+      }
+    }
+  })
+
+  console.log(wrapper.html())
+  expect(wrapper.html()).toContain('Some title')
+  expect(wrapper.find('p').html()).toContain('Parent Type')
+  expect(wrapper.find('#data').exists()).toBe(true)
+  expect(wrapper.find('#data').html()).toContain('Has Data')
+})
