80f90f889cec098da71c921bd28bc6a624db6fdb
Victor Gladkikh <me@alfeg.net>
2021-09-10 16:26:57 +0300

    fix: domWrapper.d.ts imports (#937)
    Fixes #936

--- src/domWrapper.ts
+++ src/domWrapper.ts
@@ -5,9 +5,8 @@ import { createWrapperError } from './errorWrapper'
 import WrapperLike from './interfaces/wrapperLike'
 import { ComponentInternalInstance, ComponentPublicInstance } from 'vue'
 import { FindAllComponentsSelector, FindComponentSelector } from './types'
-import { VueWrapper } from 'src'
 import { matches, find } from './utils/find'
-import { createWrapper } from './vueWrapper'
+import { VueWrapper, createWrapper } from './vueWrapper'
 
 export class DOMWrapper<ElementType extends Element>
   extends BaseWrapper<ElementType>
