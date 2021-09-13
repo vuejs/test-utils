10fbeebdc348df53c6525643299b871e0f483d37
Rick <r.aikema@ultraware.nl>
2021-09-13 10:25:51 +0200

    export createWrapperError

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { MountingOptions } from './types'
 import { RouterLinkStub } from './components/RouterLinkStub'
 import { VueWrapper } from './vueWrapper'
 import { DOMWrapper } from './domWrapper'
+import { createWrapperError } from './errorWrapper'
 import { config } from './config'
 import { flushPromises } from './utils/flushPromises'
 
@@ -14,5 +15,6 @@ export {
   DOMWrapper,
   config,
   flushPromises,
-  MountingOptions
+  MountingOptions,
+  createWrapperError
 }
