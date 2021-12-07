08cd947cbca9281fde4a4028ea81a8d0836d1f5e
Evert van der Weit <evert@mett.nl>
2021-12-07 20:49:28 +0100

    chore: make linter happy

--- tests/components/ScriptSetupWithChildren.vue
+++ tests/components/ScriptSetupWithChildren.vue
@@ -4,7 +4,9 @@ import { defineAsyncComponent } from 'vue'
 import Hello from './Hello.vue'
 import ComponentWithInput from './ComponentWithInput.vue'
 import ComponentWithoutName from './ComponentWithoutName.vue'
-const ComponentAsync = defineAsyncComponent(() => import('./ComponentWithoutName.vue'))
+const ComponentAsync = defineAsyncComponent(
+  () => import('./ComponentWithoutName.vue')
+)
 import ScriptSetup from './ScriptSetup.vue'
 import WithProps from './WithProps.vue'
 </script>
