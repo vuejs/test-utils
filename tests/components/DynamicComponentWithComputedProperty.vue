89fe6a7a82c898c8c39118bb8eac936051309160
Charles Hudson <charles.hudson@traderepublic.com>
2022-02-14 09:06:16 +0100

    test: Add failing tests for shallowMount issue with dynamic components

---
+++ tests/components/DynamicComponentWithComputedProperty.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="Hello" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import Hello from './Hello.vue'
+import ComponentWithChildren from './ComponentWithChildren.vue'
+
+interface Props {
+  isHello?: boolean
+}
+
+const props = defineProps<Props>()
+
+const computedProperty = computed(() => {
+  return props.isHello ? Hello : ComponentWithChildren
+})
+</script>
