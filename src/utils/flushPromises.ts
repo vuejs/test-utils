b0d6aab2c04f6400942eee660948141b55b6866d
Marina Mosti <marina@mosti.com.mx>
2022-03-08 13:14:37 +0100

    chore: give credit to original flushPromises lib
    Come on :P

--- src/utils/flushPromises.ts
+++ src/utils/flushPromises.ts
@@ -1,5 +1,6 @@
 const scheduler = typeof setImmediate === 'function' ? setImmediate : setTimeout
 
+// Credit to: https://github.com/kentor/flush-promises
 export function flushPromises() {
   return new Promise((resolve) => {
     scheduler(resolve, 0)
