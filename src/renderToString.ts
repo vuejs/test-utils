b2787e4aaa1914a7705954dd0d832f2375cb51da
johnsoncodehk <johnsoncodehk@gmail.com>
2023-04-11 06:01:58 +0800

    refactor: sync renderToString with mount

--- src/renderToString.ts
+++ src/renderToString.ts
@@ -1,131 +1,32 @@
 import { renderToString as baseRenderToString } from '@vue/server-renderer'
 import {
-  FunctionalComponent,
-  ComponentOptionsWithObjectProps,
-  ComponentOptionsWithArrayProps,
-  ComponentOptionsWithoutProps,
-  ExtractPropTypes,
-  VNodeProps,
-  ComponentOptionsMixin,
-  DefineComponent,
-  MethodOptions,
   AllowedComponentProps,
   ComponentCustomProps,
-  ExtractDefaultPropTypes,
-  EmitsOptions,
-  ComputedOptions,
+  ComponentOptionsMixin,
+  ComponentOptionsWithArrayProps,
+  ComponentOptionsWithObjectProps,
+  ComponentOptionsWithoutProps,
   ComponentPropsOptions,
-  Prop
+  ComputedOptions,
+  EmitsOptions,
+  ExtractPropTypes,
+  VNodeProps
 } from 'vue'
 
-import { RenderMountingOptions } from './types'
 import { createInstance } from './createInstance'
+import { ComponentMountingOptions } from './mount'
+import { RenderMountingOptions } from './types'
 
 // NOTE this should come from `vue`
 type PublicProps = VNodeProps & AllowedComponentProps & ComponentCustomProps
 
-type ComponentMountingOptions<T> = T extends DefineComponent<
-  infer PropsOrPropOptions,
-  any,
-  infer D,
-  any,
-  any
->
-  ? RenderMountingOptions<
-      Partial<ExtractDefaultPropTypes<PropsOrPropOptions>> &
-        Omit<
-          Readonly<ExtractPropTypes<PropsOrPropOptions>> & PublicProps,
-          keyof ExtractDefaultPropTypes<PropsOrPropOptions>
-        >,
-      D
-    > &
-      Record<string, any>
-  : RenderMountingOptions<any>
-
-// Class component (without vue-class-component) - no props
-export function renderToString<V extends {}>(
-  originalComponent: {
-    new (...args: any[]): V
-    __vccOpts: any
-  },
-  options?: RenderMountingOptions<any> & Record<string, any>
-): Promise<string>
-
-// Class component (without vue-class-component) - props
-export function renderToString<V extends {}, P>(
-  originalComponent: {
-    new (...args: any[]): V
-    __vccOpts: any
-    defaultProps?: Record<string, Prop<any>> | string[]
-  },
-  options?: RenderMountingOptions<P & PublicProps> & Record<string, any>
-): Promise<string>
-
-// Class component - no props
-export function renderToString<V extends {}>(
-  originalComponent: {
-    new (...args: any[]): V
-    registerHooks(keys: string[]): void
-  },
-  options?: RenderMountingOptions<any> & Record<string, any>
-): Promise<string>
-
-// Class component - props
-export function renderToString<V extends {}, P>(
-  originalComponent: {
-    new (...args: any[]): V
-    props(Props: P): any
-    registerHooks(keys: string[]): void
-  },
-  options?: RenderMountingOptions<P & PublicProps> & Record<string, any>
-): Promise<string>
-
-// Functional component with emits
-export function renderToString<Props extends {}, E extends EmitsOptions = {}>(
-  originalComponent: FunctionalComponent<Props, E>,
-  options?: RenderMountingOptions<Props & PublicProps> & Record<string, any>
-): Promise<string>
-
-// Component declared with defineComponent
 export function renderToString<
-  PropsOrPropOptions = {},
-  RawBindings = {},
-  D = {},
-  C extends ComputedOptions = ComputedOptions,
-  M extends MethodOptions = MethodOptions,
-  Mixin extends ComponentOptionsMixin = ComponentOptionsMixin,
-  Extends extends ComponentOptionsMixin = ComponentOptionsMixin,
-  E extends EmitsOptions = Record<string, any>,
-  EE extends string = string,
-  PP = PublicProps,
-  Props = Readonly<ExtractPropTypes<PropsOrPropOptions>>,
-  Defaults extends {} = ExtractDefaultPropTypes<PropsOrPropOptions>
+  T extends ((...args: any) => any) | (new (...args: any) => any)
 >(
-  component: DefineComponent<
-    PropsOrPropOptions,
-    RawBindings,
-    D,
-    C,
-    M,
-    Mixin,
-    Extends,
-    E,
-    EE,
-    PP,
-    Props,
-    Defaults
-  >,
-  options?: RenderMountingOptions<
-    Partial<Defaults> & Omit<Props & PublicProps, keyof Defaults>,
-    D
-  > &
-    Record<string, any>
+  originalComponent: T,
+  options?: ComponentMountingOptions<T> &
+    Pick<RenderMountingOptions<any>, 'attachTo'>
 ): Promise<string>
-
-// component declared by vue-tsc ScriptSetup
-export function renderToString<
-  T extends DefineComponent<any, any, any, any, any>
->(component: T, options?: ComponentMountingOptions<T>): Promise<string>
 
 // Component declared with no props
 export function renderToString<
