1b6cf217360ca86850ced0699a4c300a6ff7eeb7
johnsoncodehk <johnsoncodehk@gmail.com>
2023-04-11 06:16:40 +0800

    chore: add issue link

--- tests/components/WithTeleportEmitsComp.vue
+++ tests/components/WithTeleportEmitsComp.vue
@@ -1,7 +1,7 @@
 <template>
   <teleport to="#somewhere">
     <emits-event />
-    <!-- avoid vue-tsc unknown prop bug for now -->
+    <!-- avoid vue-tsc unknown prop bug for now, issue: https://github.com/vuejs/language-tools/issues/2584 -->
     <!-- <emits-event msg="hi there" /> -->
   </teleport>
 </template>
