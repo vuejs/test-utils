a29bdd432c17bb8407d14d76567f2628dd72d4e3
cexbrayat <cedric@ninja-squad.com>
2023-11-15 09:30:23 +0100

    chore: format with prettier v3.1

--- src/renderToString.ts
+++ src/renderToString.ts
@@ -9,12 +9,12 @@ export function renderToString<
   C = T extends ((...args: any) => any) | (new (...args: any) => any)
     ? T
     : T extends { props?: infer Props }
-    ? DefineComponent<
-        Props extends Readonly<(infer PropNames)[]> | (infer PropNames)[]
-          ? { [key in PropNames extends string ? PropNames : string]?: any }
-          : Props
-      >
-    : DefineComponent
+      ? DefineComponent<
+          Props extends Readonly<(infer PropNames)[]> | (infer PropNames)[]
+            ? { [key in PropNames extends string ? PropNames : string]?: any }
+            : Props
+        >
+      : DefineComponent
 >(
   originalComponent: T,
   options?: ComponentMountingOptions<C> &
