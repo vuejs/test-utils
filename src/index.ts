cd4903e9017d6f3eda3f537a8aca6e01bbab5763
Will <williamneal1@proton.me>
2023-11-30 11:08:01 +0100

    feat: conditionally export renderToStrings based on environment

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { DOMWrapper } from './domWrapper'
 import { VueWrapper } from './vueWrapper'
 import BaseWrapper from './baseWrapper'
 import { mount, shallowMount } from './mount'
-import { renderToString } from './renderToString'
+import { renderToString as _renderToString } from './renderToString'
 import { MountingOptions } from './types'
 import { RouterLinkStub } from './components/RouterLinkStub'
 import { createWrapperError } from './errorWrapper'
@@ -10,6 +10,9 @@ import { config } from './config'
 import { flushPromises } from './utils/flushPromises'
 import { enableAutoUnmount, disableAutoUnmount } from './utils/autoUnmount'
 
+// is __SSR__ avaialble? If so, preferable to use that?
+const isNode = typeof window === 'undefined'
+const renderToString = ( isNode ?  _renderToString  : null)
 export {
   mount,
   shallowMount,
