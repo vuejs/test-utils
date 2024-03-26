a8cc83bcd1769bd191f974efd2d1530e7edff72a
taku-y-9308 <y.takumi4@gmail.com>
2024-03-26 22:47:35 +0900

    test: Added test to access global properties

---
+++ tests/components/OptionSetupWithoutReturnComponent.vue
@@ -0,0 +1,18 @@
+<script>
+import { onUnmounted } from 'vue'
+
+export default {
+  setup() {
+    onUnmounted(() => { console.log('unmounted') })
+  },
+  data() {
+    return {
+      message: 'Hello world!'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>{{ message }} (Options API component with a setup() function that does not return)</div>
+</template>
