cda399f6cb830910b402f1fdf67b77042c1a4186
Victor Fernandez <vfernandezgabriel@zscaler.com>
2025-03-13 10:29:27 +0100

    fix: use Teleport with disabled=true instead of stub

---
+++ tests/components/TeleportRemountChild.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue'
+
+const emit = defineEmits<{ myEvent: ['mount' | 'unmount'] }>()
+
+onMounted(() => {
+  emit('myEvent', 'mount')
+})
+
+onUnmounted(() => {
+  emit('myEvent', 'unmount')
+})
+</script>
+
+<template>
+  <span>child</span>
+</template>
